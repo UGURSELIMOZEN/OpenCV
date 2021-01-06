11dcfeefb9b69fe8b63f7730b2320ad14df703d3
Uğur Selim Özen <72982905+UGURSELIMOZEN@users.noreply.github.com>
2021-01-06 23:02:00 +0300

    Update lecture_16.py

--- OpenCV_Lectures/lecture_16.py
+++ OpenCV_Lectures/lecture_16.py
@@ -1,28 +1,28 @@
 # -*- coding: utf-8 -*-
-                                            #### ARKA PLANI FILTRELEME #####
+                                            #### FILTERING THE BACKGROUND #####
 import cv2
 import numpy as np
 
 def main():
     
-    resim =cv2.imread("mecnun.jpg")
+    image =cv2.imread("mecnun.jpg")
     
-    cv2.imshow("MECNUN",resim)
+    cv2.imshow("MECNUN",image)
     
-    mask =np.zeros(resim.shape[:2],np.uint8)
+    mask =np.zeros(image.shape[:2],np.uint8)
     
     background_model =np.zeros((1,65),dtype = np.float64)
     forwardground_model =np.zeros((1,65),dtype = np.float64)
     
     rectangle = (200,0,420,400)
     
-    cv2.grabCut(resim,mask,rectangle,background_model,forwardground_model,5,cv2.GC_INIT_WITH_RECT)
+    cv2.grabCut(image,mask,rectangle,background_model,forwardground_model,5,cv2.GC_INIT_WITH_RECT)
     
     mask2= np.where((mask == 0) | (mask == 2),0,1).astype(np.uint8)
     
-    resim =resim * mask2[:,:,np.newaxis]
+    image =image * mask2[:,:,np.newaxis]
     
-    cv2.imshow("NEW_MECNUN",resim)
+    cv2.imshow("NEW_MECNUN",image)
     
     cv2.waitKey(0)
     cv2.destroyAllWindows()
