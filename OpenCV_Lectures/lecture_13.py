f81a9ee08f1f800fe075e5d56f7e79574361696a
Uğur Selim Özen <72982905+UGURSELIMOZEN@users.noreply.github.com>
2021-01-06 23:18:16 +0300

    Update lecture_13.py

--- OpenCV_Lectures/lecture_13.py
+++ OpenCV_Lectures/lecture_13.py
@@ -3,55 +3,55 @@
 import cv2
 import numpy as np
 
-kamera =cv2.VideoCapture(0)
+camera =cv2.VideoCapture(0)
 
-dusuk  = np.array([20,100,150])
-yuksek = np.array([37,255,255])     
+low  = np.array([20,100,150])
+high = np.array([37,255,255])     
 
-# dusuk  = np.array([20, 0, 0])
-# yuksek = np.array([37, 255, 255])     # sarı filtreleme için
+# low  = np.array([20, 0, 0])
+# high = np.array([37, 255, 255])     # filtering for yellow color
 
-# dusuk  = np.array([0, 0, 0])
-# yuksek = np.array([10, 255, 255])     # kırmızı filtreleme için
+# low  = np.array([0, 0, 0])
+# high = np.array([10, 255, 255])     # filtering for red color
 
-# dusuk  = np.array([85, 0, 0])
-# yuksek = np.array([100, 255, 255])     # Açık mavi filtreleme için
+# low  = np.array([85, 0, 0])
+# high = np.array([100, 255, 255])     # filtering for light blue color
 
-# dusuk  = np.array([45, 0, 0])
-# yuksek = np.array([75, 255, 255])     # yeşil filtreleme için
+# low  = np.array([45, 0, 0])
+# high = np.array([75, 255, 255])     # filtering for green color
 while True:
     
-    dogru,goruntu = kamera.read()
+    ret,frame = camera.read()
     
-    hsv =cv2.cvtColor(goruntu,cv2.COLOR_BGR2HSV)
+    hsv =cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
     
-    mask =cv2.inRange(hsv,dusuk,yuksek)
+    mask =cv2.inRange(hsv,low,high)
     
-    son_resim = cv2.bitwise_and(goruntu,goruntu,mask = mask)
+    last_image = cv2.bitwise_and(frame,frame,mask = mask)
     
     ## 1.SMOOTHED FILTER
 #    kernel = np.ones((15,15),dtype= np.float32) / 255
-#    smoothed =cv2.filter2D(son_resim,-1,kernel)
+#    smoothed =cv2.filter2D(last_image,-1,kernel)
     
     ## 2.BLUR FILTER
-#    blur =cv2.GaussianBlur(son_resim,(15,15),0)
+#    blur =cv2.GaussianBlur(last_image,(15,15),0)
     
     ## 3.MEDIAN FILTER
-    median =cv2.medianBlur(son_resim,15)        ###### EN IYI FILTRE BU DIYEBILIRIZ
+    median =cv2.medianBlur(last_image,15)        ###### may be best choice for the filter types
     
     ## 4.BILATERAL FILTER
-#    bilateral = cv2.bilateralFilter(son_resim,15,15,75)
+#    bilateral = cv2.bilateralFilter(last_image,15,15,75)
     
     
    # cv2.imshow("SMOOTHED FİLTER",smoothed)
    # cv2.imshow("BLUR FILTER",blur)
     cv2.imshow("MEDIANBLUR FILTER",median)
    # cv2.imshow("BILATERAL FILTER",bilateral)
-   # cv2.imshow("Filtresiz goruntu",son_resim)
+   # cv2.imshow("Not filtered frame",last_image)
     
     
     if cv2.waitKey(10)  & 0xFF == ord('e'):
         break
     
-kamera.release()
+camera.release()
 cv2.destroyAllWindows()
