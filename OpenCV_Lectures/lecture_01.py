8f450a18c68e4f877af5d0731904dc1ca98cca12
Uğur Selim Özen <72982905+UGURSELIMOZEN@users.noreply.github.com>
2021-01-06 22:36:18 +0300

    Update lecture_01.py

--- OpenCV_Lectures/lecture_01.py
+++ OpenCV_Lectures/lecture_01.py
@@ -4,7 +4,7 @@ import numpy as np
 
 image  = cv2.imread("download.jpg")
 image2 = cv2.imread("download.jpg",0) ##to make your image as gray##
-print(type(resim))
+print(type(image))
 cv2.imwrite("download1.jpg",image2)
 cv2.imshow("First_Image",image)
 cv2.imshow("Gray_Image",image2)
