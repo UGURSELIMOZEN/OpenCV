df425ddc815cb133647d29b3bc79136e905953ac
Uğur Selim Özen <72982905+UGURSELIMOZEN@users.noreply.github.com>
2021-01-06 23:26:45 +0300

    Update lecture_10.py

--- OpenCV_Lectures/lecture_10.py
+++ OpenCV_Lectures/lecture_10.py
@@ -3,36 +3,36 @@
 import cv2
 import numpy as np
 
-kamera = cv2.VideoCapture(0)
+camera = cv2.VideoCapture(0)
 
-# kamera.set(3,200)    #  bu kısmı genişliği set eder
-# kamera.set(4,700)    # bu kısmı yukseklıgi set eder
+# camera.set(3,200)    #  to set width in the video
+# camera.set(4,700)    # to set heigth in the video
 
-def ayarlama (kare,yuzde =75):
-    width  = int(kare.shape[1]*yuzde/100)
-    height = int(kare.shape[0]*yuzde/100)
+def arranging (frame,percent =75):
+    width  = int(frame.shape[1]*percent/100)
+    height = int(frame.shape[0]*percent/100)
     shape=(width,height)
-    return cv2.resize(kare,shape,interpolation =cv2.INTER_AREA)
+    return cv2.resize(frame,shape,interpolation =cv2.INTER_AREA)
 
 
 
 def main():
     while True:
         
-        ret,kare = kamera.read()
+        ret,frame = camera.read()
         
-        gri_img = cv2.cvtColor(kare,cv2.COLOR_BGR2GRAY) # resmi grileştirmek için
+        gray_img = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY) # to  make the image as gray
         
-        resim2= ayarlama (kare,60)
+        image2= arranging (frame,60)
         
-        cv2.imshow("gray_img",gri_img)
-        cv2.imshow("ayarlanan resim",resim2)
-        cv2.imshow("usb_cam",kare)
+        cv2.imshow("gray_img",gray_img )
+        cv2.imshow("arranged image",image2)
+        cv2.imshow("usb_cam",frame)
         
         if cv2.waitKey(25) & 0xFF == ord('e'):
             break
         
-    kamera.release()
+    camera.release()
     cv2.destroyAllWindows()     
 
 if __name__ == "__main__":
