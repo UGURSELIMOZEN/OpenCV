689108a1d64eb4d242a373de9aab9215ba54a497
Uğur Selim Özen <72982905+UGURSELIMOZEN@users.noreply.github.com>
2021-01-06 22:58:11 +0300

    Update lecture_17_b.py

--- OpenCV_Lectures/lecture_17_b.py
+++ OpenCV_Lectures/lecture_17_b.py
@@ -3,23 +3,23 @@
 import cv2
 import numpy as np
 
-kamera = cv2.VideoCapture(0)
+camera = cv2.VideoCapture(0)
 
 while True:
-    ret,kare = kamera.read()
+    ret,square = camera.read()
     
-    laplacian = cv2.Laplacian(kare,cv2.CV_64F)
+    laplacian = cv2.Laplacian(square,cv2.CV_64F)
     
-    canny = cv2.Canny(kare,50,100)
+    canny = cv2.Canny(square,50,100)
     
     cv2.imshow("laplacian_filter",laplacian)
     
     cv2.imshow("canny_filter",canny)
     
-    cv2.imshow("orjinal",kare)
+    cv2.imshow("original",square)
     
     if cv2.waitKey(25) & 0xFF == ord('e'):
         break
     
-kamera.release()
+camera.release()
 cv2.destroyAllWindows() 
