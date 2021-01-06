d3086b6de04a41fdaeff34a055a372ea973459b2
Uğur Selim Özen <72982905+UGURSELIMOZEN@users.noreply.github.com>
2021-01-06 23:00:14 +0300

    Update lecture_17_a.py

--- OpenCV_Lectures/lecture_17_a.py
+++ OpenCV_Lectures/lecture_17_a.py
@@ -3,18 +3,18 @@
 import cv2
 import numpy as np
 
-resim = cv2.imread("mecnun.jpg")
+image = cv2.imread("mecnun.jpg")
 
-laplacian = cv2.Laplacian(resim,cv2.CV_64F)
+laplacian = cv2.Laplacian(image,cv2.CV_64F)
 
-sobel_dikey =cv2.Sobel(resim,cv2.CV_64F,1,0,ksize = 5)
+sobel_vertical =cv2.Sobel(image,cv2.CV_64F,1,0,ksize = 5)
 
-sobel_yatay =cv2.Sobel(resim,cv2.CV_64F,0,1,ksize = 5)
+sobel_horizontal =cv2.Sobel(image,cv2.CV_64F,0,1,ksize = 5)
 
-##  cv2.imshow("sobel_yatay",sobel_yatay)
-cv2.imshow("sobel_dikey",sobel_dikey)
+##  cv2.imshow("sobel_vertical",sobel_horizontal)
+cv2.imshow("sobel_vertical",sobel_vertical)
 cv2.imshow("laplacian",laplacian)
-cv2.imshow("orjinal",resim)
+cv2.imshow("original",image)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
