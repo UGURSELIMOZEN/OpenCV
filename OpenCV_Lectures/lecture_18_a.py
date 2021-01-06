90dd7c491a643fa0b3b34e3b497d53321c8ca8d0
Uğur Selim Özen <72982905+UGURSELIMOZEN@users.noreply.github.com>
2021-01-06 22:56:55 +0300

    Update lecture_18_a.py

--- OpenCV_Lectures/lecture_18_a.py
+++ OpenCV_Lectures/lecture_18_a.py
@@ -5,20 +5,20 @@ import numpy as np
 
 def main():
     
-    resim = cv2.imread("figures.jpg")
-    gri_resim = cv2.cvtColor(resim,cv2.COLOR_BGR2GRAY)
-    gri_resim = np.float32(gri_resim)
+    image = cv2.imread("figures.jpg")
+    gray_image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    gray_image = np.float32(gray_image)
     
     
-    corners = cv2.goodFeaturesToTrack(gri_resim,30,0.1,65)
+    corners = cv2.goodFeaturesToTrack(gray_image,30,0.1,65)
     corners = np.int0(corners)
     
     for corner in corners:
         x,y =corner.ravel()
-        cv2.circle(resim,(x,y),3,(255,0,0),-1)
+        cv2.circle(image,(x,y),3,(255,0,0),-1)
         
         
-    cv2.imshow("corner_detection",resim)
+    cv2.imshow("corner_detection",image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     
